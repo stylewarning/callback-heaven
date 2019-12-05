6d52807130efef23f886b533a977ed3594567c4c
Mark Skilbeck <mark.skilbeck@rigetti.com>
2019-12-05 13:25:39 -0800

    Comment out run-time specific code in example.lisp

--- example/example.lisp
+++ example/example.lisp
@@ -4,6 +4,8 @@
 
 (in-package #:callback-heaven-examples)
 
+;; First define your API group, and its API functions.
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (define-api-group example))
 
@@ -21,31 +23,48 @@
 
 (defvar *ctrans* (compute-c-space-translation (api-group 'example)))
 
-;; (emit-library-files *ctrans* "example.c" "example.h")
+;; If your above API changes, you need to re-emit the C-API with the
+;; following
+;; 
+;;   (emit-library-files *ctrans* "example.c" "example.h")
 ;;
-;;; Make sure you're CD'd into the current directory.
+;; Then you will need to recompile the shared library
+;;
+;;   make clean; make
 
+;; To load the shared library, make sure you are in the same directory
+;; as the library and load it with:
+;; 
+;; (cffi:define-foreign-library example
+;;   (:darwin "libexample.dylib")
+;;   (:unix   "libexample.so")
+;;   (t (:default "libexample")))
+;; 
+;; (cffi:use-foreign-library example)
 
-
-(cffi:define-foreign-library example
-  (:darwin "libexample.dylib")
-  (:unix   "libexample.so")
-  (t (:default "libexample")))
-
-(cffi:use-foreign-library example)
-
-(cffi:defcfun ("set_function_index_group_example" %set-function-index-group-example)
-    :void
-  (functions (:pointer (:pointer :void))))
-
-(defun set-group-example-index ()
-  (%set-function-index-group-example
-   (callback-heaven::c-space-translation-function-index *ctrans*)))
-
+;; CALLBACK-HEAVEN will also define a function (in the .c/.h files)
+;; with the name set_function_index_group_<api-group-name>.  This
+;; function will take in pointers to our defined lisp functions
+;; (above) and set-up the trampoline structure in C.  We need to
+;; inform CFFI of its existence:
+;; 
+;; (cffi:defcfun ("set_function_index_group_example" %set-function-index-group-example)
+;;     :void
+;;   (functions (:pointer (:pointer :void))))
+;; 
+;; (defun set-group-example-index ()
+;;   (%set-function-index-group-example
+;;    (callback-heaven::c-space-translation-function-index *ctrans*)))
+;; 
 ;; (set-group-example-index)
 
-(cffi:defcfun ("call_me_from_lisp" %call-me-from-lisp) :void)
-
+;; In main.c we defined a function call_me_from_lisp, that will act as
+;; our go-between.  Tell CFFI of its existence,
+;; 
+;; (cffi:defcfun ("call_me_from_lisp" %call-me-from-lisp) :void)
+;;
+;; and invoke it with:
+;; 
 ;; CALLBACK-HEAVEN-EXAMPLES> (%call-me-from-lisp)
 ;; Factorial 5 = 120
 ;; NIL
